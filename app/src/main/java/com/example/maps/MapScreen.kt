f154e66bb1699b7533e10f0798d5299d3257a487
Marco <marco.rios.c@tecsup.edu.pe>
2024-11-13 14:04:07 -0500

    Añadir marcador personalizado en Arequipa Perú

--- app/src/main/java/com/example/maps/MapScreen.kt
+++ app/src/main/java/com/example/maps/MapScreen.kt
@@ -1,11 +1,14 @@
 package com.example.maps
 
+import android.graphics.BitmapFactory
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
@@ -15,6 +18,7 @@ import com.google.maps.android.compose.Marker
 
 @Composable
 fun MapScreen() {
+    val context = LocalContext.current
     val ArequipaLocation = LatLng(-16.4040102, -71.559611) // Arequipa, Perú
     val cameraPositionState = rememberCameraPositionState {
         position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(ArequipaLocation, 12f)
@@ -27,9 +31,15 @@ fun MapScreen() {
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
-            // Añadir marcador en Denver, Colorado
+
+            val customIcon = BitmapDescriptorFactory.fromBitmap(
+                BitmapFactory.decodeResource(context.resources, R.drawable.montana)
+            )
+
+            // Añadir marcador en Arequipa Perú
             Marker(
                 state = rememberMarkerState(position = ArequipaLocation),
+                icon = customIcon,
                 title = "Arequipa, Perú"
             )
         }
