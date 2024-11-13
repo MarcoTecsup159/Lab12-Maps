d923c350e947bd6123d299bcef39912c44976f3c
Marco <marco.rios.c@tecsup.edu.pe>
2024-11-13 15:04:36 -0500

    Agregar poligonos

--- app/src/main/java/com/example/maps/MapScreen.kt
+++ app/src/main/java/com/example/maps/MapScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
@@ -16,6 +17,8 @@ import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.Polygon
+import com.google.maps.android.compose.Polyline
 
 
 @Composable
@@ -55,12 +58,70 @@ fun MapScreen() {
                 )
             }
 
-            LaunchedEffect(Unit) {
+            //Mover la camara a Yura
+            /*LaunchedEffect(Unit) {
                 cameraPositionState.animate(
                     update = CameraUpdateFactory.newLatLngZoom(LatLng(-16.2520984,-71.6836503), 12f), // Mover a Yura
                     durationMs = 3000
                 )
-            }
+            }*/
+
+            val mallAventuraPolygon = listOf(
+                LatLng(-16.432292, -71.509145),
+                LatLng(-16.432757, -71.509626),
+                LatLng(-16.433013, -71.509310),
+                LatLng(-16.432566, -71.508853)
+            )
+
+
+            val parqueLambramaniPolygon = listOf(
+                LatLng(-16.422704, -71.530830),
+                LatLng(-16.422920, -71.531340),
+                LatLng(-16.423264, -71.531110),
+                LatLng(-16.423050, -71.530600)
+            )
+
+            val plazaDeArmasPolygon = listOf(
+                LatLng(-16.398866, -71.536961),
+                LatLng(-16.398744, -71.536529),
+                LatLng(-16.399178, -71.536289),
+                LatLng(-16.399299, -71.536721)
+            )
+
+            Polygon(
+                points = plazaDeArmasPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 10f
+            )
+            Polygon(
+                points = parqueLambramaniPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 10f
+            )
+            Polygon(
+                points = mallAventuraPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 10f
+            )
+
+            //Polilineas
+            val polylinePoints = listOf(
+                LatLng(-16.4040102, -71.559611), // Punto inicial
+                LatLng(-16.433415, -71.5442652), // JLByR
+                LatLng(-16.4205151, -71.4945209), // Paucarpata
+                LatLng(-16.3524187, -71.5675994) // Zamacola
+            )
+
+            Polyline(
+                points = polylinePoints,
+                color = Color.Green,
+                width = 15f
+            )
+
+
         }
     }
 }
